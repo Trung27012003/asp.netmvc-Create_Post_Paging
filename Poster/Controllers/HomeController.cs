c440ba828f33f602c63a956b6e061394070f03df
Trung27012003 <Trungtvph28671@fpt.edu.vn>
2023-05-20 14:09:22 +0700

    update search nangcao

--- Poster/Controllers/HomeController.cs
+++ Poster/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Poster.Models;
 using System.Diagnostics;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Poster.Controllers
 {
@@ -19,12 +20,41 @@ namespace Poster.Controllers
 
             return RedirectToAction("ShowListPosts");
     }
-         public IActionResult ShowListPosts([FromQuery(Name = "page")]int CurrentPage)
-        {
-            int PageSize = 10;
-            var post = _context.Posts.ToList();
-            int TotalPost = post.Count(); // tổng số bài
-            int CountPages = (int)Math.Ceiling((double)TotalPost / PageSize);
+         public IActionResult ShowListPosts([FromQuery(Name = "page")]int CurrentPage, [FromQuery] string? searchKeyword, [FromQuery] string? title, [FromQuery] string? content, [FromQuery] string? orderby, [FromQuery] int? minprice, [FromQuery] int? maxprice)
+         {
+            // khi tìm mới thì reset lọc
+
+            int PageSize = 10; // 10 items
+            var post = _context.Posts.ToList(); // lấy list post
+            title = title ?? "";
+            content = content ?? "";
+            if (!string.IsNullOrEmpty(searchKeyword)) //check từ khóa tìm kiếm (title)
+            {
+                post = post.Where(p => p.Title.Contains(searchKeyword)).ToList();
+            }
+            if (title!=null) // check title
+            {
+                post = post.Where(p => p.Title.Contains(title)).ToList();
+			}
+            if (content!=null) //check content
+            {
+                post = post.Where(p => p.Content.Contains(content)).ToList();
+			}
+            if (minprice != null && maxprice!=null) // check minprice và maxprice
+            {
+                post.Where(p => Int32.Parse(p.Title) <= maxprice && Int32.Parse(p.Title) >= minprice);
+
+			}
+			if (orderby == "ASC") 
+			{
+				post = post.OrderBy(c=>c.Title).ToList();
+			}
+			if (orderby == "DESC")
+			{
+				post = post.OrderByDescending(c => c.Title).ToList();
+			}
+			int TotalPost = post.Count(); // tổng số bài
+            int CountPages = (int)Math.Ceiling((double)TotalPost / PageSize); 
             if (CurrentPage>CountPages)
             {
                 CurrentPage = CountPages;
@@ -37,25 +67,35 @@ namespace Poster.Controllers
             {
                 CurrentPage = CurrentPage,
                 CountPage = CountPages,
-                GeneralUrl = (pageNumber)=>Url.Action("ShowListPosts", new
+                GeneralUrl = (pageNumber) => Url.Action("ShowListPosts", new
                 {
                     page = pageNumber,
+                    searchKeyword = searchKeyword,
+                    title = title,
+                    content = content,
+                    orderby = orderby,
+                    minprice = minprice,
+                    maxprice = maxprice
                 })
             };
             ViewBag.Paging = Paging;
             ViewBag.TotalPost = TotalPost;
-            var PostInPage = post.Skip((CurrentPage-1)*PageSize).Take(10).ToList();
-            return View(PostInPage);
+            ViewBag.orderby = orderby; // của select
+			ViewData["minprice"] = minprice;
+			ViewData["maxprice"] = maxprice;
+			ViewData["searchKeyword"] = searchKeyword;
+			var PostInPage = post.Skip((CurrentPage-1)*PageSize).Take(10).ToList();
+			return View(PostInPage);
         }
 
         public IActionResult Them()
         {
-            for (int i = 0; i < 500; i++)
+            for (int i = 0; i < 1000; i++)
             {
                 Posts p = new Posts()
                 {
                     Content = "Content thứ: " + i,
-                    Title = "Bài viết số : " +i,
+                    Title =i.ToString(),
                     Published = true
                 };
                 _context.Posts.Add(p);
@@ -65,7 +105,7 @@ namespace Poster.Controllers
         }
         public IActionResult Xoa()
         {
-            var post = _context.Posts.Take(500).ToList();
+            var post = _context.Posts.Take(1000).ToList();
             foreach (var item in post)
             {
                 _context.Remove(item);
